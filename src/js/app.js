4cbaac4c4cde475df002683bc957e472471b61c0
Mayia Rodina <designw657@gmail.com>
2025-12-03 13:57:18 +0300

    fix app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,18 @@
 const btn = document.querySelector(".button");
 const field = document.querySelector(".text-field");
 
-btn.addEventListener("click", () => {
-  field.classList.toggle("expanded");
-});
+const onClick = () => {
+    field.classList.toggle("expanded");
+  };
+
+const onLoad = () => {
+  btn.addEventListener("click", onClick);
+};
+
+const onBeforeUnload = () => {
+  btn.removeEventListener('click', onClick);
+  document.removeEventListener('DOMContentLoaded', onLoad);
+}
+
+document.addEventListener('DOMContentLoaded', onLoad);
+document.addEventListener('beforeunload', onBeforeUnload);
